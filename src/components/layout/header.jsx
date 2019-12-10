497b607d936e67753ef8c931a2d5986870a35510
Jonathan <kakashi169@gmail.com>
2019-12-10 02:35:36 +0000

    added the header with title and search bar. Title probably will be changed later

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -2,13 +2,41 @@ import React from 'react';
 import './layout.css';
 
 export default class Header extends React.Component {
-    render() {
-        return (
-            <div className="header">
-                <div className="headerTitle">
-                    <p>Reference Check App</p>
-                </div>
-            </div>
-        )
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			searchField: ""
+		}
+	}
+
+	handleSearch(event) {
+		const searchTarget = event.target.value;
+		this.setState({ searchField: searchTarget });
+	}
+
+	render() {
+		return (
+			<nav className="navbar navbar-dark bg-secondary">
+				<div className="navbar-brand">
+						Reference Check
+				</div>
+				<form className="form-inline">
+					<div className="input-group">
+						<div className="input-group-prepend">
+							<span className="input-group-text" id="basic-addon1">
+								<i className="fas fa-search"></i>
+							</span>
+						</div>
+						<input 
+							type="text" 
+							className="form-control" 
+							placeholder="Search" 
+							value={this.state.searchField} 
+							aria-describedby="basic-addon1"
+							onChange={event => this.handleSearch(event)} />
+					</div>
+				</form>
+			</nav>
+		)
+	}
 }
