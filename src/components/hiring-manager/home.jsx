4d86f0f0c9756e1d3c3d3271dc9f9ac5eb6ff219
Jonathan <kakashi169@gmail.com>
2019-12-10 14:18:49 +0000

    added tab navigation through employer home page

---
+++ src/components/hiring-manager/home.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import Candidates from './tab-candidates';
+import Dashboard from './tab-dashboard';
+import OpenJobs from './tab-open-jobs';
+
+export default class Home extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      currentTab: 'dashboard'
+    }
+    this.handleTabClick = this.handleTabClick.bind(this);
+  }
+
+  handleTabClick(event) {
+    const tab = event.currentTarget.getAttribute('name');
+    console.log('value is', tab);
+    this.setState({ currentTab: tab })
+  } 
+
+  render() {
+    let currentTab;
+    switch (this.state.currentTab) {
+      case "dashboard":
+        currentTab = <Dashboard />
+        break;
+      case "openJobs":
+        currentTab = <OpenJobs />
+        break;
+      case "candidates":
+        currentTab = <Candidates />
+    }
+
+    return (
+      <div className="container-fluid">
+        <ul className="nav nav-tabs mt-2">
+          <li className="nav-item">
+            <a className="nav-link active" name="dashboard" data-toggle="tab" onClick={this.handleTabClick}> 
+              Dashboard 
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" name="openJobs" data-toggle="tab" onClick={this.handleTabClick}>
+              Open Jobs
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" name="candidates" data-toggle="tab" onClick={this.handleTabClick}>
+              Candidates
+            </a>
+          </li>
+        </ul>
+        {currentTab}
+    </div>
+    )
+  }
+}
---
+++ src/components/hiring-manager/tab-candidates.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class Candidates extends React.Component {
+  render() {
+    return (
+      <div>candidates window</div>
+    )
+  }
+}
