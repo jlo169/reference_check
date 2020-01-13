e6c8376021c663bc7a89c4a95b12d7c20eb33192
Jonathan <kakashi169@gmail.com>
2020-01-13 03:10:36 +0000

    added checkbox, need to connect to backend

--- src/components/hiring-manager/form-add-job.jsx
+++ src/components/hiring-manager/form-add-job.jsx
@@ -1,18 +1,43 @@
 import React from 'react';
+import TargetSkills from './targetSkillsCheckbox';
 
 export default class AddJob extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      jobTitle: 'asdf',
-      focusArea: 'asdf',
-      salary: 1
+      jobTitle: '',
+      focusArea: '',
+      salary: 0,
+      jobMission: '',
+      desiredOutcomes1: '',
+      desiredOutcomes2: '',
+      desiredOutcomes3: '',
+      targetSkills: {
+        productSensitivity: false,
+        strategy: false,
+        projectManagement: false,
+        design: false,
+        dataExecution: false,
+        communication: false,
+        technical: false,
+        userResearch: false,
+        charisma: false
+      }
     }
+
+    this.onCheckboxClick = this.onCheckboxClick.bind(this);
   }
 
   handleInputs(event) {
     const { name, value } = event.target;
     this.setState({ [name]: value })
+  }
+
+  onCheckboxClick(event) {
+    const value = event.target.checked;
+    const name = event.target.name;
+    const updatedTargetSkills = Object.assign({}, this.state.targetSkills, {[name]: value});
+    this.setState({targetSkills: updatedTargetSkills});
   }
 
   submitData() {
@@ -48,15 +73,59 @@ export default class AddJob extends React.Component {
             <div className="mt-3">
             Salary
               <input
-                type="text"
+                type="number"
                 name="salary"
                 className="col-md-12"
                 value={this.state.salary}
                 onChange={event => this.handleInputs(event)}
               />
             </div>
+            <div className="mt-3">
+            Job Mission
+              <input
+                type="text"
+                name="jobMission"
+                className="col-md-12"
+                value={this.state.jobMission}
+                onChange={event => this.handleInputs(event)}
+              />
+            </div>
+            <div className="mt-3">
+            Desired Outcomes #1
+              <input
+                type="text"
+                name="desiredOutcomes1"
+                className="col-md-12"
+                value={this.state.desiredOutcomes}
+                onChange={event => this.handleInputs(event)}
+              />
+            </div>
+            <div className="mt-3">
+            Desired Outcomes #2
+              <input
+                type="text"
+                name="desiredOutcomes2"
+                className="col-md-12"
+                value={this.state.desiredOutcomes}
+                onChange={event => this.handleInputs(event)}
+              />
+            </div>
+            <div className="mt-3">
+            Desired Outcomes #3
+              <input
+                type="text"
+                name="desiredOutcomes3"
+                className="col-md-12"
+                value={this.state.desiredOutcomes}
+                onChange={event => this.handleInputs(event)}
+              />
+            </div>
           </div>
         </div>
+        <TargetSkills 
+          skillState={this.state.targetSkills}
+          checkboxClick={this.onCheckboxClick}
+        />
         <button onClick={() => this.submitData()}>submit</button>
       </div>
     );
---
+++ src/components/hiring-manager/targetSkillsCheckbox.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+
+export default class TargetSkills extends React.Component {
+  renderTargetSkills() {
+    const skills = [
+      {title: 'Product Sensitivity',
+      name: 'productSensitivity'}, 
+      {title: 'Strategy',
+      name: 'strategy'}, 
+      {title: 'Project Management',
+      name: 'projectManagement'}, 
+      {title: 'Design',
+      name: 'design'}, 
+      {title: 'Data/KPI Execution',
+      name: 'dataExecution'}, 
+      {title: 'Communication',
+      name: 'communication'}, 
+      {title: 'Technical',
+      name: 'technical'}, 
+      {title: 'User Research',
+      name: 'userResearch'}, 
+      {title: 'Charisma',
+      name: 'charisma'}
+    ];
+    return skills.map( (skill, i) => {
+      return (
+        <div className="form-check" key={i}>
+          <input 
+            className="form-check-input" 
+            type="checkbox" 
+            name={skill.name}
+            value={this.props.skillState[skill.name]} 
+            id={`targetSkill${i}`} 
+            onChange={this.props.checkboxClick}
+          />
+          <label className="form-check-label" htmlFor={`targetSkill${i}`}>
+            {skill.title}
+          </label>
+        </div>
+      )
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        Top 3 Target Skills
+        {this.renderTargetSkills()}
+      </div>
+    )
+  }
+}
