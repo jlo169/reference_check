fa49413c59cd155f0439382e197335de0f8b381f
Jonathan <kakashi169@gmail.com>
2020-01-16 04:47:31 +0000

    added full job to the state sent to job candidates page

--- src/components/hiring-manager/open-jobs/tab-open-jobs.jsx
+++ src/components/hiring-manager/open-jobs/tab-open-jobs.jsx
@@ -9,7 +9,7 @@ export default class OpenJobs extends React.Component {
     super(props)
     this.state = {
       currentTable: 'jobs',
-      jobListing: '',
+      jobListing: [],
       candidates: []
     }
 
@@ -17,14 +17,15 @@ export default class OpenJobs extends React.Component {
   }
 
   getJobCandidates(event) {
-    const jobId = event.target.getAttribute('value');
-    const jobListing = event.target.innerText;
+    const jobId = parseInt(event.target.getAttribute('value'));
+    const jobListing = this.props.jobListings.filter(job => job.id === jobId);
 
     axios.get(`/api/openJobs/${jobId}`)
       .then(response => {
+        console.log(response.data)
         this.setState({ 
           currentTable: 'candidates', 
-          jobListing: jobListing,
+          jobListing: jobListing[0],
           candidates: response.data
         })
       })
@@ -32,24 +33,23 @@ export default class OpenJobs extends React.Component {
   }
 
   handleBackArrowClick() {
-    this.setState({ currentTable: 'jobs', jobListing: '' });
+    this.setState({ currentTable: 'jobs', jobListing: [] });
   }
 
   render() {
     return (
       <div className="container-fluid">
         <div className="row justify-content-between my-3">
+        {this.state.jobListing.id ? (
           <div className="row col-xs-12 col-sm-4 ml-2">
-            <h3 className="font-weight-bold my-0 ml-2">{this.state.jobListing}</h3>
-            {this.state.jobListing ? (
+            <h3 className="font-weight-bold my-0 ml-2">{this.state.jobListing.jobTitle}</h3>
               <h4 className="text-secondary ml-3 mt-1"><i className="fas fa-arrow-left" onClick={() => this.handleBackArrowClick()}></i></h4>
-            ) : (
-              <div></div>
-            )}
           </div>
-          <div className="row col-xs-12 col-sm-4 justify-content-end">
+        ) : (
+          <div className="row col-12 justify-content-end">
             <button type="button" className="btn btn-primary mx-3" onClick={() => this.props.addJob()}>Add Open Job</button>
           </div>
+        )}
         </div>
         {this.state.currentTable === 'jobs' ? (
           <JobsTable 
