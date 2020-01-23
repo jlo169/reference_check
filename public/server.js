529d9a3e5236fb8299e45d8dbbdb90ec3c5552e0
Jonathan <kakashi169@gmail.com>
2020-01-23 13:50:28 +0000

    added express-session package, only studied on how to implement

--- public/server.js
+++ public/server.js
@@ -5,6 +5,7 @@ const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
 const path = require('path');
+const session = require('express-session');
 const app = express();
 const PORT = process.env.DB_PORT || 3001;
 
